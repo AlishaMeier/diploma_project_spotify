001a4136c33c80399004137891f14ce4139c232b
AlishaMeier <alisha.meyer@gmail.com>
2025-10-24 19:26:56 +0500

    fix invalid login data

--- tests/ui/test_login_web.py
+++ tests/ui/test_login_web.py
@@ -13,23 +13,6 @@ INVALID_LOGIN_DATA = [
     ("whoami@c.com", "любой_пароль", ERROR_NOT_LINKED)
 ]
 
-@pytest.mark.ui
-@allure.feature("Авторизация")
-@allure.story("Тест на успешную авторизацию")
-@allure.label("owner", "AlishaMeier")
-@allure.tag("positive", "critical")
-@allure.severity(allure.severity_level.CRITICAL)
-def test_login(credentials, login_page: LoginPage, navigation_page: NavigationPage):
-    with allure.step("Переход на страницу авторизации с главной"):
-        navigation_page.navigate_to_login()
-        current_url = browser.driver.current_url
-        print(f"DEBUG: Current URL after navigate_to_login: {current_url}")
-
-    with allure.step("Выполнение полной авторизации"):
-        login_page.login(credentials["username"], credentials["password"])
-
-    with allure.step("Проверка успешного входа"):
-        login_page.should_be_logged_in()
 
 @pytest.mark.ui
 @allure.feature("Авторизация")
@@ -87,3 +70,22 @@ def test_invalid_login_with_parameters(username, password, expected_error, login
 
     with allure.step("Проверить отображение сообщения об ошибке: '{expected_error}'"):
         login_page.should_see_generic_error_message(expected_error)
+
+
+@pytest.mark.ui
+@allure.feature("Авторизация")
+@allure.story("Тест на успешную авторизацию")
+@allure.label("owner", "AlishaMeier")
+@allure.tag("positive", "critical")
+@allure.severity(allure.severity_level.CRITICAL)
+def test_login(credentials, login_page: LoginPage, navigation_page: NavigationPage):
+    with allure.step("Переход на страницу авторизации с главной"):
+        navigation_page.navigate_to_login()
+        current_url = browser.driver.current_url
+        print(f"DEBUG: Current URL after navigate_to_login: {current_url}")
+
+    with allure.step("Выполнение полной авторизации"):
+        login_page.login(credentials["username"], credentials["password"])
+
+    with allure.step("Проверка успешного входа"):
+        login_page.should_be_logged_in()
